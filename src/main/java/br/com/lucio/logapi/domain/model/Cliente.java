c50bd9c83897ababefd034f954e892193a135d98
Rodrigo Lúcio <56269484+rodrigo-lucio@users.noreply.github.com>
2021-05-28 00:26:13 -0300

    Criado model Cliente

---
+++ src/main/java/br/com/lucio/logapi/domain/model/Cliente.java
@@ -0,0 +1,42 @@
+package br.com.lucio.logapi.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import br.com.lucio.logapi.domain.ValidationGroups;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cliente {
+
+	@NotNull(groups = ValidationGroups.ClienteId.class)	//Grupo de validação apenas para essa entidade
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Size(max = 60)
+	private String nome;
+	
+	@NotBlank
+	@Email
+	@Size(min = 10, max = 255)
+	private String email;
+	
+	@NotBlank
+	@Size(max = 20)
+	private String telefone;
+
+}
