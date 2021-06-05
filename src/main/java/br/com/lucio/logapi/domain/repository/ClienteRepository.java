af67fa5de82805e46307de27b5c446ad35e98a82
Rodrigo Lúcio <56269484+rodrigo-lucio@users.noreply.github.com>
2021-06-05 10:40:40 -0300

    Criado repositório de cliente

---
+++ src/main/java/br/com/lucio/logapi/domain/repository/ClienteRepository.java
@@ -0,0 +1,15 @@
+ package br.com.lucio.logapi.domain.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.lucio.logapi.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+	
+	Optional<Cliente> findByEmail(String email);
+	
+}
