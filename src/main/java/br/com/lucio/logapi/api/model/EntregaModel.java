2680d1175a8bc09a23ff72d3337b8f8d5eda93ea
Rodrigo Lúcio <56269484+rodrigo-lucio@users.noreply.github.com>
2021-06-14 20:54:16 -0300

    Criado EntregaModel

---
+++ src/main/java/br/com/lucio/logapi/api/model/EntregaModel.java
@@ -0,0 +1,25 @@
+package br.com.lucio.logapi.api.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+import br.com.lucio.logapi.domain.model.StatusEntrega;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EntregaModel {
+
+	private Long id;
+    private ClienteResumoModel cliente;
+	private DestinatarioModel destinatario;
+	private BigDecimal taxa;
+	private StatusEntrega status;
+	private OffsetDateTime dataPedido;
+	private OffsetDateTime dataFinalizacao;
+	
+	private List<OcorrenciaModel> ocorrencias; 
+	
+}
